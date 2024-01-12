0dfdb4647ee6cf3bfd03c1cb5fdcfc8b036c2516
saketh139 <114804334+saketh139@users.noreply.github.com>
2024-01-12 10:44:37 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# here iam importing the modules in calculations
+
+import calculations
+x=2
+y=3
+
+
+z=calculations.add(x,y)
+print(z)
+
+z=calculations.mul(x,y)
+print(z)
+
+z=calculations.div(x,y)
+print(z)
+
+z=calculations.sub(x,y)
+print(z)
